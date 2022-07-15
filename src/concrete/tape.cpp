f053e2d8b927a16854d7342a5679bcc6c5fd6533
luwae <l.waeldele@t-online.de>
2022-07-15 14:27:17 +0200

    concrete: use size_type in tape

--- src/concrete/tape.cpp
+++ src/concrete/tape.cpp
@@ -5,20 +5,20 @@ using std::vector;
 
 unsigned char &Tape::operator[](int index) {
     if (index >= 0) {
-        if ((vector<unsigned char>::size_type) index >= right.size())
-            right.insert(right.end(), index - right.size() + 1, '_');
+        if ((size_type) index >= right.size())
+            right.insert(right.end(), index - (int) right.size() + 1, '_');
         return right[index];
     }
     index = -index - 1;
-    if ((vector<unsigned char>::size_type) index >= left.size())
-        left.insert(left.end(), index - left.size() + 1, '_');
+    if ((size_type) index >= left.size())
+        left.insert(left.end(), index - (int) left.size() + 1, '_');
     return left[index];
 }
 
-ostream &operator<<(ostream &os, const Tape &t) {
-    for (auto i = t.left.rbegin(); i != t.left.rend(); ++i)
+ostream &operator<<(ostream &os, const Tape &tape) {
+    for (auto i = tape.left.rbegin(); i != tape.left.rend(); ++i)
         os << *i;
-    for (unsigned char c : t.right)
+    for (unsigned char c : tape.right)
         os << c;
     return os;
 }
--- src/concrete/tape.hpp
+++ src/concrete/tape.hpp
@@ -7,14 +7,15 @@
 
 class Tape {
 public:
-    friend std::ostream &operator<<(std::ostream &os, const Tape &t);
+    using size_type = std::vector<unsigned char>::size_type;
     Tape() = default;
     Tape(const std::string &input): right(input.begin(), input.end()) { }
     Tape(const std::vector<unsigned char> &v): right(v.begin(), v.end()) { }
     unsigned char &operator[](int index);
-    int size() const { return right.size() + left.size(); }
-    int rsize() const { return right.size(); }
-    int lsize() const { return left.size(); }
+    size_type size() const { return right.size() + left.size(); }
+    size_type rsize() const { return right.size(); }
+    size_type lsize() const { return left.size(); }
+    friend std::ostream &operator<<(std::ostream &os, const Tape &tape);
 private:
     std::vector<unsigned char> right;
     std::vector<unsigned char> left;
